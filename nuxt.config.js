d115b99870d5a8980a794920823bbf64fc951663
Tanay Pant <7481165+tanay1337@users.noreply.github.com>
2022-01-13 21:41:26 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
       okta: {
         scheme: "openIDConnect",
         endpoints: {
-          configuration: `https://${process.env.OKTA_DOMAIN}/oauth2/default/.well-known/oauth-authorization-server`,
+          configuration: `${process.env.OKTA_DOMAIN}/oauth2/default/.well-known/oauth-authorization-server`,
           logout: undefined,
         },
         clientId: process.env.OKTA_CLIENT_ID,
